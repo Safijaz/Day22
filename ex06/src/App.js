96515e0ee0b829792a2de60ff8f6e43b49f6aadd
Safijaz <safija.zahirovic@gmail.com>
2021-03-30 00:24:27 +0200

    commit

---
+++ ex06/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Comonent} from 'react';
+import  Plant from'./Plant';
+
+function App() {
+  return (
+    <div className="App">
+      <Plant />
+      
+    </div>
+  );
+}
+
+export default App;
