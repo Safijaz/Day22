138ad6bbcb25f919da02cc24dc4408f2d5bb15ca
Safijaz <safija.zahirovic@gmail.com>
2021-03-29 22:14:09 +0200

    commit

---
+++ ex04/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+import Box from './Box';
+
+function App() {
+  return (
+    <div className="App">
+      <Box />
+      
+    </div>
+  );
+}
+
+export default App;
